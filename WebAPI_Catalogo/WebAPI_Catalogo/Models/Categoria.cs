fe62c74ab6f3be64b5aa960ba816a2100811b2b3
Addam Reis <addamreis@gmail.com>
2020-09-09 19:40:04 -0300

    Added nuget packages and defined models

---
+++ WebAPI_Catalogo/WebAPI_Catalogo/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Catalogo.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
