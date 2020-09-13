ca718a4081bc750d424fbef6631d7301a071d4ce
Addam Reis <addamreis@gmail.com>
2020-09-13 14:00:58 -0300

    Defining controller (Get ID)

--- WebAPI_Catalogo/WebAPI_Catalogo/Controllers/ProdutosController.cs
+++ WebAPI_Catalogo/WebAPI_Catalogo/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,8 @@ using WebAPI_Catalogo.Models;
 namespace WebAPI_Catalogo.Controllers
 {
     //ActionResult retorna os status possíveis as requisições (no caso de API)
-    //IEnumerable<> expõe um enumerador que suporta uma interação entre uma lista de objetos
+    //IEnumerable<> expõe um enumerador que suporta uma interação entre uma lista de objetos (retorno de lista)
+    //AsnoTraking só pode ser utilizado em consultas, o mesmo aumenta o desempenho da operação.
     [Route("api/[Controller]")]
     [ApiController]
     public class ProdutosController : ControllerBase
@@ -23,7 +25,17 @@ namespace WebAPI_Catalogo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-           return _context.Produtos.ToList();
+           return _context.Produtos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+            if (produto == null)
+                return NotFound();
+
+            return produto;
         }
     }
 }
