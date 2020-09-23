038873bd77d6f0f40eef108d86ddcffc178fc039
Addam Reis <addamreis@gmail.com>
2020-09-23 19:19:36 -0300

    AutorizaController | Register and Login

---
+++ WebAPI_Catalogo/WebAPI_Catalogo/Controllers/AutorizaController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_Catalogo.DTOs;
+
+namespace WebAPI_Catalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AutorizaController :: Acessado em : " + DateTime.Now.ToLongDateString();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] UsuarioDTO model) //recebe no corpo as informações do usuário
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password); //criando instancia do usuário
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            await _signInManager.SignInAsync(user, false);
+            return Ok(/*GeraToken(model)*/);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UsuarioDTO userInfo)
+        {
+            //verifica se as credenciais do usuário retorna um valor
+            var result = await _signInManager.PasswordSignInAsync
+                (userInfo.Email, userInfo.Password, isPersistent: false, lockoutOnFailure: false); //lockoutOnFailure: se tentar mais de 3 vezes bloqueia
+
+            if (result.Succeeded)
+                return Ok();
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login inválido...");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
